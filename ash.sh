75f472291600c69f177ad03586de9c2b5f61ac55
anvian <andre.carbajal.vargas@gmail.com>
2021-10-26 20:59:32 -0500

    errors fixed

---
+++ ash.sh
@@ -0,0 +1,5 @@
+#remove the greeting 
+set fish_greeting
+
+#install a custom theme | https://github.com/oh-my-fish/oh-my-fish/blob/master/docs/Themes.md
+omf install boxfish 
--- script.sh
+++ script.sh
@@ -4,9 +4,39 @@ sudo apt update && sudo apt upgrade - y
 #install kde
 sudo apt install kde-plasma-desktop -y
 
+#install aptitude
+sudo apt install aptitude -y
+
+#isntall NodeJa
+sudo aptitude install nodejs -y
+
+#install VsCode
+sudo snap install code --classic
+
+#install neofetch
+sudo apt install neofetch -y
+neofetch
+
+#uninstall firefox and install brave
+sudo apt purge firefox -y
+rm -r .mozilla/
+cd /etc
+rm -r firefox/
+cd /usr/lib
+rm -r mozilla/
+sudo snap install brave
+
+#uninstall dolphin
+sudo apt purge dolphin
+
+#unistall konqueror
+sudo apt purge konqueror
+
+#install filezilla
+sudo aptitude install filezilla -y
+
 #insatall and configure fish shell
 sudo apt install fish -y
-set fish_greeting
 sudo apt install curl -y
 sudo apt install fonts-powerline -y
 
@@ -14,6 +44,3 @@ sudo apt install fonts-powerline -y
 chsh -s /usr/bin/fish
 
 curl -L https://get.oh-my.fish | fish
-
-#reboot the sistem
-sudo reboot
