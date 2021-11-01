6e6ea9992def2c16f60799b6d5c02fa84b248880
anviaan <andre.carbajal.vargas@gmail.com>
2021-11-01 15:01:20 -0500

    replace brave with edge

--- script.sh
+++ script.sh
@@ -28,7 +28,14 @@ cd /etc
 rm -r firefox/
 cd /usr/lib
 sudo rm -r mozilla/
-sudo snap install brave
+cd
+
+#install Edge
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+sudo rm microsoft.gpg
+sudo apt update && sudo apt install microsoft-edge-stable
 
 #uninstall dolphin
 sudo apt purge dolphin
@@ -41,11 +48,12 @@ sudo aptitude install filezilla -y
 
 #insatall and configure fish shell
 sudo apt install fish -y
+
 #config fish file
-mv config.fish ~/.config/fish/
+mv ./config.fish ~/.config/fish/
+
 #set fish shell
 chsh -s /usr/bin/fish
 sudo apt install curl -y
 sudo apt install fonts-powerline -y
-
 curl -L https://get.oh-my.fish | fish
