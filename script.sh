6ed6652c2232392c947245bf27bb31b3e40291e3
anviaan <andre.carbajal.vargas@gmail.com>
2022-04-25 20:05:15 -0500

    remove kde, add some programs, etc

--- script.sh
+++ script.sh
@@ -1,9 +1,6 @@
 #update and upgrade
 sudo apt update && sudo apt upgrade - y
 
-#install kde
-sudo apt install kde-plasma-desktop -y
-
 #install aptitude
 sudo apt install aptitude -y
 
@@ -13,35 +10,20 @@ sudo aptitude install nodejs -y
 #install npm
 sudo apt install npm -y
 
-#install java16
-sudo aptitude install openjdk-16-jdk -y
+#install java17
+sudo aptitude install openjdk-17-jdk -y
+
+#install snap 
+sudo apt install snap
 
 #install VsCode
 sudo snap install code --classic
 
+#install intellij ide
+sudo snap install intellij-idea-community --classic
+
 #install neofetch
 sudo apt install neofetch -y
-
-#uninstall firefox and install brave
-sudo apt purge firefox -y
-cd /etc
-rm -r firefox/
-cd /usr/lib
-sudo rm -r mozilla/
-cd
-
-#install Edge
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
-sudo rm microsoft.gpg
-sudo apt update && sudo apt install microsoft-edge-stable
-
-#uninstall dolphin
-sudo apt purge dolphin
-
-#unistall konqueror
-sudo apt purge konqueror
 
 #install filezilla
 sudo aptitude install filezilla -y
